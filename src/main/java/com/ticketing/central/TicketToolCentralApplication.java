31d5b2c1618617602ab091cad10162140d1fef1a
Jan Geschwandtner <geschwandtner.jan@gmail.com>
2022-05-13 23:22:20 +0200

    initial commit

---
+++ src/main/java/com/ticketing/central/TicketToolCentralApplication.java
@@ -0,0 +1,13 @@
+package com.ticketing.central;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketToolCentralApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketToolCentralApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ticketing/central/TicketToolCentralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketing.central;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketToolCentralApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
